5262ceb8b2cc1c988376e70e10b7ca8b1f77230e
HKprogram <ricknogueira1231@gmail.com>
2022-06-13 11:59:11 -0300

    W02 - Criando controle principal do App

---
+++ src/controller/WhatsAppController.js
@@ -0,0 +1,20 @@
+class WhatsAppController {
+
+    constructor() {
+
+        console.log("WhatsAppController OK");
+
+        this.loadElements();
+
+    }
+
+    loadElements() {
+
+        this.el = {};
+
+        document.querySelectorAll('[id]').forEach(element => {
+            this.el[Format.getCamelCase(element.id)] = element;
+        });
+    }
+
+}
