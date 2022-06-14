c4f5296650e53e0a5175c171c9426cb3f5b02bf0
HKprogram <ricknogueira1231@gmail.com>
2022-06-14 18:02:34 -0300

    W05 - Obtendo dados dos painéis e usando FormData

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -70,6 +70,20 @@ class WhatsAppController {
             return this.classList.contains(name);
         }
 
+        HTMLFormElement.prototype.getForm = function() {
+            return new FormData(this);
+        }
+        
+        HTMLFormElement.prototype.toJSON = function() {
+            let json = {};
+
+            this.getForm().forEach((value, key) => {
+                json[key] = value;
+            });
+
+            return json;
+        }
+
     }
 
     initElements() {
@@ -96,7 +110,37 @@ class WhatsAppController {
         this.el.btnClosePanelAddContact.on('click', e => {
             
             this.el.panelAddContact.removeClass('open');
-        })
+        });
+
+        this.el.photoContainerEditProfile.on('click', e => {
+
+            this.el.inputProfilePhoto.click();
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+
+            if(e.key === 'Enter') {
+
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+
+            }
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+        });
+
+        this.el.formPanelAddContact.on('click', e => {
+
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact);
+
+        });
+
     }
 
     closeAllLeftPanel() {
