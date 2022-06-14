008f26af62da960181fbfa4928b5cda248fbcfdf
HKprogram <ricknogueira1231@gmail.com>
2022-06-14 11:37:34 -0300

    W04 - Eventos para abrir/fechar os painéis Editar Perfil e Adicionar Contato

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -6,6 +6,7 @@ class WhatsAppController {
 
         this.elementsPrototype();
         this.loadElements();
+        this.initElements();
 
     }
 
@@ -69,6 +70,38 @@ class WhatsAppController {
             return this.classList.contains(name);
         }
 
+    }
+
+    initElements() {
+        this.el.myPhoto.on('click', e => {
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+        });
+
+        this.el.btnNewContact.on('click', e => {
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+            this.el.panelEditProfile.removeClass('open');
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+            
+            this.el.panelAddContact.removeClass('open');
+        })
+    }
+
+    closeAllLeftPanel() {
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
     }
 
 }
