53dc8f7419c093bdecc2da3c3633ca54703c1009
HKprogram <ricknogueira1231@gmail.com>
2022-06-15 15:58:35 -0300

    W06 - Clique no Menu Anexar - Usando bind() e removeEventListener()

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -141,6 +141,51 @@ class WhatsAppController {
 
         });
 
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+
+            item.on('click', e => {
+                this.el.home.hide();
+                this.el.main.css({
+                    display: 'flex',
+                })
+            });
+
+        });
+
+        this.el.btnAttach.on('click', e => {
+            
+            e.stopPropagation();
+            
+            this.el.menuAttach.addClass("open");
+
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+
+        });
+
+        this.el.btnAttachPhoto.on('click', e => {
+            console.log('photo');
+        });
+
+        this.el.btnAttachCamera.on('click', e => {
+            console.log('camera');
+        });
+
+        this.el.btnAttachDocument.on('click', e => {
+            console.log('document');
+        })
+
+        this.el.btnAttachContact.on('click', e => {
+            console.log('contact');
+        });
+
+    }
+
+    closeMenuAttach(e) {
+
+        document.removeEventListener('click', this.closeMenuAttach);
+
+        this.el.menuAttach.removeClass("open");
+
     }
 
     closeAllLeftPanel() {
