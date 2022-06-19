46bf04756ff93dd93827d46dcab9d58fbc819e71
soapmactavish23 <ricknogueira1231@gmail.com>
2022-06-19 11:41:51 -0300

    W08 - Eventos de gravação do microfone e timer de gravação

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -212,15 +212,47 @@ class WhatsAppController {
         });
 
         this.el.btnAttachContact.on('click', e => {
-            
+
             this.el.modalContacts.show();
 
         });
 
         this.el.btnCloseModalContacts.on('click', e => {
             this.el.modalContacts.hide();
-        })
+        });
 
+        this.el.btnSendMicrophone.on('click', e => {
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTime();
+        });
+
+        this.el.btnCancelMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        });
+
+        this.el.btnFinishMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        });
+
+    }
+
+    startRecordMicrophoneTime() {
+
+        let start = Date.now();
+
+        this._recordMicrophoneInterval = setInterval(() => {
+            
+            this.el.recordMicrophoneTimer.innerHTML = (Date.now() - start);
+
+        }, 100);
+
+    }
+
+    closeRecordMicrophone() {
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._recordMicrophoneInterval);
     }
 
     closeAllMainPanel() {
