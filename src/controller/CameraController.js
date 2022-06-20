be7dc42743e4f8210bdac4433595a442e7e63d07
HKprogram <ricknogueira1231@gmail.com>
2022-06-20 15:58:41 -0300

    W13 - Ativando a câmera com API MediaDevices.getUserMedia()

---
+++ src/controller/CameraController.js
@@ -0,0 +1,18 @@
+class CameraController {
+
+    constructor(videoEl) {
+        this._videoEl = videoEl;
+
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then(stream => {
+
+            this._videoEl.src = URL.createObjectURL(stream);
+            this._videlEl.play();
+
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+
+}
