b8e2945531ef02e1e773e9aeeaf63e8049230fa1
soapmactavish23 <ricknogueira1231@gmail.com>
2022-06-19 22:18:29 -0300

    W10 - Eventos do campo Digitar Mensagem

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -235,6 +235,39 @@ class WhatsAppController {
             this.closeRecordMicrophone();
         });
 
+        this.el.inputText.on('keypress', e => {
+            if(e.key === 'Enter' && !e.ctrlKey) {
+                e.preventDefault();
+                this.el.btnSend.click();
+            }
+        });
+
+        this.el.inputText.on('keyup', e => {
+            if (this.el.inputText.innerHTML.length) {
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+            } else {
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+            }
+        });
+
+        this.el.btnSend.on('click', e => {
+            console.log(this.el.inputText.innerHTML);
+        });
+
+        this.el.btnEmojis.on('click', e => {
+            this.el.panelEmojis.toggleClass('open');
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+            emoji.on('click', e => {
+                console.log(emoji.dataset.unicode);
+            });
+        });
+
     }
 
     startRecordMicrophoneTime() {
@@ -242,7 +275,7 @@ class WhatsAppController {
         let start = Date.now();
 
         this._recordMicrophoneInterval = setInterval(() => {
-            
+
             this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
 
         }, 100);
