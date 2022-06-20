881414a58b6b26278e9931c39964f8ba10aac3fb
HKprogram <ricknogueira1231@gmail.com>
2022-06-20 15:52:48 -0300

    W12 - Inserir Emoji getSelection(), createRange() e DocumentFragment()

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -236,7 +236,7 @@ class WhatsAppController {
         });
 
         this.el.inputText.on('keypress', e => {
-            if(e.key === 'Enter' && !e.ctrlKey) {
+            if (e.key === 'Enter' && !e.ctrlKey) {
                 e.preventDefault();
                 this.el.btnSend.click();
             }
@@ -275,7 +275,22 @@ class WhatsAppController {
                     img.classList.add(name);
                 });
 
-                this.el.inputText.appendChild(img);
+                let cursor = window.getSelection();
+
+                if (!cursor.focusNode || !cursor.focusNode.id == 'input-text') {
+                    this.el.inputText.focus();
+                }
+
+                let range = document.createRange();
+
+                range = cursor.getRangeAt(0);
+                range.deleteContents();
+
+                let frag = document.createDocumentFragment();
+                frag.appendChild(img);
+                
+                range.insertNode(frag);
+                range.setStartAfter(img);
 
                 this.el.inputText.dispatchEvent(new Event('keyup'));
 
