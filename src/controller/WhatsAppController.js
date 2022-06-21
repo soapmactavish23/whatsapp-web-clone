719f2ff8dfdcb6f3abd6abef1f2c35bb1875e00f
HKprogram <ricknogueira1231@gmail.com>
2022-06-21 14:00:28 -0300

    W18 - Tirar fotografia da câmera com canvas

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -195,7 +195,32 @@ export class WhatsAppController {
         });
 
         this.el.btnTakePicture.on('click', e => {
-            console.log('take picture');
+            
+            let dataUrl = this._camera.takePicture();
+
+            this.el.pictureCamera.src = dataUrl;
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();
+
+        });
+
+        this.el.btnReshootPanelCamera.on('click', e => {
+
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
+
+        });
+
+        this.el.btnSendPicture.on('click', e => {
+
+            console.log(this.el.pictureCamera.src);
+
         });
 
         this.el.btnAttachDocument.on('click', e => {
