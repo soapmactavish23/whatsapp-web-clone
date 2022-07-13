f7820bc59cb70d2284484bca85ff0b45c8866d56
HKprogram <ricknogueira1231@gmail.com>
2022-07-12 22:52:19 -0300

    W41 - Enviando foto como anexo

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -186,14 +186,14 @@ export class WhatsAppController {
                 let message = new Message();
 
                 message.fromJSON(data);
-                
+
                 let me = (data.from === this._user.email);
 
                 if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)) {
 
 
                     if (!me) {
-                        
+
                         setDoc(doc.ref, { status: 'read' }, { merge: true });
                     }
 
@@ -201,7 +201,7 @@ export class WhatsAppController {
 
                     this.el.panelMessagesContainer.appendChild(view);
 
-                } else if(me) {
+                } else if (me) {
 
                     let msgEl = this.el.panelMessagesContainer.querySelector('#_' + data.id);
 
@@ -384,7 +384,7 @@ export class WhatsAppController {
                             this.el.btnClosePanelAddContact.click();
                             console.info('Contato foi adicionado');
                         })
-                        
+
                         contact.addContact(this._user);
                     });
 
@@ -470,7 +470,48 @@ export class WhatsAppController {
 
         this.el.btnSendPicture.on('click', e => {
 
-            console.log(this.el.pictureCamera.src);
+            this.el.btnSendPicture.disabled = true;
+
+            let regex = /^data:(.+);base64,(.*)$/;
+            let result = this.el.pictureCamera.src.match(regex);
+            let mimeType = result[1];
+            let ext = mimeType.split('/')[1];
+            let filename = `camera${Date.now()}.${ext}`;
+
+            let picture = new Image();
+            picture.src = this.el.pictureCamera.src;
+            picture.onload = (e) => {
+
+                let canvas = document.createElement('canvas');
+                let context = canvas.getContext('2d');
+
+                canvas.width = picture.width;
+                canvas.height = picture.height;
+
+                context.translate(picture.width, 0);
+                context.scale(-1, 1);
+
+                context.drawImage(picture, 0, 0, canvas.width, canvas.height);
+
+                fetch(canvas.toDataURL(mimeType))
+                .then(res => { return res.arrayBuffer(); })
+                .then(buffer => { return new File([buffer], filename, { type: mimeType }); })
+                .then(file => {
+                    Message.sendImage(this._contactActive.chatId, this._user.email, file);
+
+                    this.el.btnSendPicture.disabled = false;
+
+                    this.closeAllMainPanel();
+                    this._camera.stop();
+                    this.el.btnReshootPanelCamera.hide();
+                    this.el.pictureCamera.hide();
+                    this.el.videoCamera.show();
+                    this.el.containerSendPicture.hide();
+                    this.el.containerTakePicture.show();
+                    this.el.panelMessagesContainer.show();
+
+                })
+            }
 
         });
 
