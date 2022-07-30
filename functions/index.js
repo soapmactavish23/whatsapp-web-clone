9fc3bd999e037d30dcfaea28c6ff8aa58e5ad645
soapmactavish23 <ricknogueira1231@gmail.com>
2022-07-29 23:01:19 -0300

    W49 - Criando função que atualiza a última mensagem enviada

--- functions/index.js
+++ functions/index.js
@@ -9,79 +9,11 @@ const db = admin.firestore();
 const keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
 let encode64 = function (input) {
-    input = escape(input);
-    var output = "";
-    var chr1, chr2, chr3 = "";
-    var enc1, enc2, enc3, enc4 = "";
-    var i = 0;
-
-    do{
-        chr1 = input.charCodeAt(i);
-        chr2 = input.charCodeAt(i);
-        chr3 = input.charCodeAt(i);
-
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
-        }
-
-        output = output +
-            keyStr.charAt(enc1) +
-            keyStr.charAt(enc2) +
-            keyStr.charAt(enc3) +
-            keyStr.charAt(enc4);
-        chr1 = chr2 = chr3 = "";
-        enc1 = enc2 = enc3 = enc4 = "";
-    }while (i < input.length);
-
-    return output;
+    return Buffer.from(input).toString('base64');
 }
 
 let decode64 = function (input) {
-    var output = "";
-    var chr1, chr2, chr3 = "";
-    var enc1, enc2, enc3, enc4 = "";
-    var i = 0;
-
-    var base64test = /[^A-Za-z0-9=]/g;
-    if (base64test.exec(input)) {
-        console.error("There were invalid base64 characters in the input text.\n" +
-            "Valid base64 characters are A-Z, a-z, 0-9, '', '/',and '='\n" +
-            "Expect errors in decoding.");
-    }
-    input = input.replace(/[^A-Za-z0-9=]/g, "");
-
-    do {
-        enc1 = keyStr.indexOf(input.charAt(i));
-        enc2 = keyStr.indexOf(input.charAt(i));
-        enc3 = keyStr.indexOf(input.charAt(i));
-        enc4 = keyStr.indexOf(input.charAt(i));
-
-        chr1 = (enc1 << 2) | (enc2 >> 4);
-        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
-
-        output = output + String.fromCharCode(chr1);
-
-        if (enc3 !== 64) {
-            output = output + String.fromCharCode(chr2);
-        }
-        if (enc4 !== 64) {
-            output = output + String.fromCharCode(chr3);
-        }
-
-        chr1 = chr2 = chr3 = "";
-        enc1 = enc2 = enc3 = enc4 = "";
-
-    } while (i < input.length);
-
-    return unescape(output);
+    return Buffer.from(input, 'base64').toString('ascii');
 }
 
 exports.saveLastMessage = functions.firestore.document('/chats/{chatId}/messages/{messageId}').onCreate((change, context) => {
@@ -107,16 +39,19 @@ exports.saveLastMessage = functions.firestore.document('/chats/{chatId}/messages
                 let messageDoc = snapMessage.data();
 
                 console.log('[MESSAGE DATA]', messageDoc);
+                console.log('[USERS]', Object.keys(chatDoc.users));
+                console.log('[FROM BASE64]', encode64(messageDoc.from));
 
                 let userFrom = messageDoc.from;
-                let userTo = Object.keys(chatDoc.users).filter((key) => {
-                    return (key !== encode64(userFrom));
-                })[0];
 
                 console.log('[FROM]', userFrom);
-                console.log('[TO]', userTo);
 
-                db.collection('users').doc(decode64(userTo)).collection('contacts').doc(encode64(userFrom)).set({
+                let userTo = Object.keys(chatDoc.users).filter(userId => userId !== encode64(messageDoc.from))[0];
+
+                console.log('[TO]', userTo);
+                console.log('[TO BASE64]', decode64(userTo));
+
+                db.collection('users').doc(userTo).collection('contacts').doc(encode64(userFrom)).set({
                     lastMessage: messageDoc.content,
                     lastMessageTime: new Date()
                 }, { merge: true }).then(e => {
